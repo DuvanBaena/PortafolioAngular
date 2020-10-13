e6c45fadd784bd3f4ea45b79cd1cbaf88178ef5c
Duvan Baena <djbpuerta@hotmail.com>
2020-10-13 14:02:02 -0500

    15. Editando el menú de navegación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,10 +8,10 @@ import {
 }from "./components/index.paginas";
 
 const app_routes: Routes = [
-    {path: 'routePath', component: PortafolioComponent},
+    {path: 'home', component: PortafolioComponent},
     {path: 'about', component: AboutComponent},
     {path: 'product', component: ProductComponent},
-    {path: '**', pathMatch: 'full', redirectTo: ''}
+    {path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
 
 export const app_routing = RouterModule.forRoot(app_routes, {useHash:true});
